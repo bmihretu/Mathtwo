3106be9c7f7e8329b5222c160256408fd6c3facd
bereket <motta4259@gmail.com>
2019-06-12 11:28:57 -0400

    first commit

---
+++ src/mathtwo.java
@@ -0,0 +1,13 @@
+public class mathtwo {
+
+        public mathtwo() {
+        }
+
+        public static void main(String[] args) {
+            int mynumber = 2;
+            double myothernumber = 1.7938D;
+            System.out.println("mynumber is " + mynumber);
+            System.out.println("myothernumber is " + myothernumber);
+        }
+    }
+    
